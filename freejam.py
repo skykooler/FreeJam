382a2fa40561fac5e1d97fd214ed648bc096c497
Skyler Lehmkuhl <skycooler@gmail.com>
2012-02-16 00:53:51 -0500

    Added MIDI input

--- freejam.py
+++ freejam.py
@@ -1,9 +1,17 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 
+import os
+
 import pyglet
 
 from pyglet import clock
+
+import threading
+
+import math
+
+import MIDI
 
 pyglet.resource.path = ['instruments/keyboards/piano1']
 pyglet.resource.reindex()
@@ -18,6 +26,13 @@ clock.set_fps_limit(128)
 clock.schedule_interval_soft(draw,0.0625)
 
 c = pyglet.resource.media('c.wav', streaming=False)
+
+def note(val):
+	val-=36
+	if 0<=val<12:
+		return NOTE_RATIOS[val]
+	else:
+		return (2**(int(val)/12))*NOTE_RATIOS[val%12]
 
 class silence(object):
 	def __getitem__(self,item):
@@ -46,7 +61,6 @@ class track(object):
 			else:
 				b+=len(i)
 
-			
 		
 class subtrack(object):
 	def __len__(self):
@@ -61,6 +75,13 @@ class subtrack(object):
 				player.pitch = i
 				player.play()
 		return (index==len(self.data)-1)
+		
+		
+
+
+def log(message):
+	pass
+
 
 track1 = subtrack()
 track1.data = [[0.5,1],None,None,[0.561,1.122],[0.63,1.26],
@@ -77,6 +98,17 @@ TRACKS[1].add(track2)
 
 TRACKSINDEX = [0]
 
+'''def sysexcb(bs): print 'sysex: ', map(ord, bs)
+
+m = MIDI.In(sysexcb)
+
+midifile = os.open('/dev/midi1', os.O_RDONLY)
+
+def testfunc(*args):
+	print args
+
+m['note-on'] = testfunc'''
+
 def play(dt):
 	global INDEX
 	print INDEX,"\t",
@@ -84,7 +116,43 @@ def play(dt):
 		i.play(INDEX)
 	if INDEX<46:	#TODO: calculate this value
 		INDEX+=1
+		
+import MIDI
+
+def sysex(bs): print 'sysex', map(ord, bs)
+
+m = MIDI.In(sysex)
+
+def foo(msg, *args):
+	if msg=="note-on":
+		print msg, "(", args[0], args[1], args[2], ")"
+		if args[2]>1:
+			player = pyglet.media.ManagedSoundPlayer()
+			player.queue(c)
+			player.pitch = note(args[1])
+			player.volume = (args[2]/128.0)**2
+			player.play()
+	else:
+		log(msg)
+
+m[...] = foo
+
+#import os
+
+midifile = os.open('/dev/midi1', os.O_RDONLY)
+
+
+class input(threading.Thread):
+	def run(self):
+		while 1:
+			bs = os.read(midifile, 1)
+			if not bs: break
+			m.inbytes(bs)
+	
+inp = input()
 
 pyglet.clock.schedule_interval(play, 1/4.0)
+
+inp.start()
 
 pyglet.app.run()
